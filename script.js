8c4ca0351a7607205d4457e90cffb7a3c4408320
Kamal-Baraili <kamalbaraili86@gmail.com>
2025-04-24 19:34:33 +0545

    JS

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const change = document.body;
+
 function handleClick() {
   return alert("The button has been clicked");
 }
+
+const handleFocus = () => {
+  return (change.style.backgroundColor = "red");
+};
+
+change.addEventListener("click", handleFocus);
